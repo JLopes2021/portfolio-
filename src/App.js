4dc756a417bf59d0005831a4bea9fd36d5682b9b
JLopes2021 <jeffinho.je8@gmail.com>
2023-06-03 14:36:20 -0300

    atualizacao-footer

--- src/App.js
+++ src/App.js
@@ -44,6 +44,48 @@ function App() {
       <section id="blog">?????????</section>
       <section id="contact">FALA AÍ, TERRA!</section>
 
+      <footer className="nav_footer">
+        <nav className="nav__container__actions_footer">
+          <ul className={styles.ul}>
+            <li>
+              <Link activeClass="active" smooth spy to="about">
+                SOBRE MIM
+              </Link>
+            </li>
+            <li>
+              <Link activeClass="active" smooth spy to="projects">
+                PROJETOS
+              </Link>
+            </li>
+            <li>
+              <Link activeClass="active" smooth spy to="blog">
+                ????????
+              </Link>
+            </li>
+            <li>
+              <Link activeClass="active" smooth spy to="contact">
+                MEUS CONTATOS
+              </Link>
+            </li>
+            <li>
+            </li>
+          </ul>
+
+          
+        </nav>
+        <ul className={styles.ul_right}>
+            <li>
+              Item 1
+            </li>
+            <li>
+              Item 2
+            </li>
+            <li>
+              Item 3
+            </li>
+          </ul>
+      </footer>
+
     </div>
   );
 }
